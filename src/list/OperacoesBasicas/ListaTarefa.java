eedc7fcac1bfa408eebd80449dfeb885220197de
Rafael Santos <rafoliael@gmail.com>
2023-09-27 19:11:15 -0300

    feat: testando métodos do exemplo Lista de Tarefas

--- src/list/OperacoesBasicas/ListaTarefa.java
+++ src/list/OperacoesBasicas/ListaTarefa.java
@@ -31,4 +31,22 @@ public class ListaTarefa {
     public void obterDescricoesTarefas() {
         System.out.println(tarefaList);
     }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        listaTarefa.adicionarTarefa("Tarefa 4");
+        listaTarefa.adicionarTarefa("Tarefa 5");
+        listaTarefa.adicionarTarefa("Tarefa 5");
+        System.out.println("O número total de tarefas: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 5");
+        System.out.println("O número total de tarefas: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+
+    }
 }
