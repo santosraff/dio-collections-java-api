663db6e009925309e2d200eb58d7349a88edc239
Rafael Santos <rafoliael@gmail.com>
2023-09-30 20:49:58 -0300

    exemplo de ordenação de Set

---
+++ src/set/Ordenacao/CadastroProdutos.java
@@ -0,0 +1,46 @@
+package set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(codigo, nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparetorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+    public static void main(String[] args) {
+        CadastroProdutos cadastroProdutos = new CadastroProdutos();
+
+        cadastroProdutos.adicionarProduto(1L, "Smartphone", 1000d, 10);
+        cadastroProdutos.adicionarProduto(2L, "Notebook", 1500d, 5);
+        cadastroProdutos.adicionarProduto(1L, "Mouse", 30d, 20);
+        cadastroProdutos.adicionarProduto(4L, "Teclado", 50d, 15);
+
+        System.out.println("Produtos:");
+        System.out.println(cadastroProdutos.produtoSet + "\n");
+
+        System.out.println("Produtos ordenados por nome:");
+        System.out.println(cadastroProdutos.exibirProdutosPorNome() + "\n");
+
+        System.out.println("Produtos ordernados por preço:");
+        System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+    }
+}
